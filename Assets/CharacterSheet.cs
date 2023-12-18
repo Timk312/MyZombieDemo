4c7f2196157d8f549dc795e9a1cce926d737110d
tim kingston <timkingston312@gmail.com>
2023-12-17 22:35:32 -0500

    changes

--- Assets/CharacterSheet.cs
+++ Assets/CharacterSheet.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSheet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int health;
+    public int numOfHearts;
 
-    // Update is called once per frame
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
     void Update()
     {
-        
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
     }
 }
